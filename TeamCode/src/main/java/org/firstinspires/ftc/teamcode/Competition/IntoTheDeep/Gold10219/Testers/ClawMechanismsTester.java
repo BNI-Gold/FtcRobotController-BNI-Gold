28c9b68bbab6a03aa5de52257081ceb1d7fb4170
Mitchell Peck <50884550+MitchellPeck@users.noreply.github.com>
2024-10-18 21:01:33 -0400

    oops updated claw mechanisms tester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Gold10219/Testers/ClawMechanismsTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Gold10219/Testers/ClawMechanismsTester.java
@@ -15,6 +15,7 @@ public class ClawMechanismsTester extends OpMode {
 
     public Bot Bot = new Bot();
 
+    @Override
     public void init() {
         Bot.initRobot(hardwareMap);
     }
@@ -23,29 +24,38 @@ public class ClawMechanismsTester extends OpMode {
     double secondaryExtenderJump = .05;
     double clawJump = .05;
 
+    @Override
     public void loop() {
+        control();
+        telemetry();
+    }
+
+    public void control() {
         double primaryExtenderPos = Bot.primaryExtender.getPosition();
         double secondaryExtenderPos = Bot.secondaryExtender.getPosition();
         double clawPos = Bot.claw.getPosition();
         if (gamepad1.right_bumper) {
             Bot.primaryExtender.setPosition(primaryExtenderPos + primaryExtenderJump);
-            telemetry.addData("Primary Extender Position: ", Bot.primaryExtender.getPosition());
         } else if (gamepad1.left_bumper) {
             Bot.primaryExtender.setPosition(primaryExtenderPos + primaryExtenderJump);
-            telemetry.addData("Primary Extender Position: ", Bot.primaryExtender.getPosition());
         } else if (gamepad1.a) { //Should be bottom button
             Bot.secondaryExtender.setPosition(secondaryExtenderPos + secondaryExtenderJump);
-            telemetry.addData("Secondary Extender Position: ", Bot.secondaryExtender.getPosition());
         } else if (gamepad1.y) { //Should be top button
             Bot.secondaryExtender.setPosition(secondaryExtenderPos - secondaryExtenderJump);
-            telemetry.addData("Secondary Extender Position: ", Bot.secondaryExtender.getPosition());
         } else if (gamepad1.x) { //Should be left button
             Bot.claw.setPosition(clawPos + clawJump);
-            telemetry.addData("Claw Position: ", Bot.claw.getPosition());
         } else if (gamepad1.b) { //Should be right button
             Bot.claw.setPosition(clawPos - clawJump);
-            telemetry.addData("Claw Position: ", Bot.claw.getPosition());
         }
+    }
+
+    public void telemetry() {
+        telemetry.addData("Primary Extender Position: ", Bot.primaryExtender.getPosition());
+        telemetry.addData("Primary Extender Position: ", Bot.primaryExtender.getPosition());
+        telemetry.addData("Secondary Extender Position: ", Bot.secondaryExtender.getPosition());
+        telemetry.addData("Secondary Extender Position: ", Bot.secondaryExtender.getPosition());
+        telemetry.addData("Claw Position: ", Bot.claw.getPosition());
+        telemetry.addData("Claw Position: ", Bot.claw.getPosition());
         telemetry.update();
     }
 
