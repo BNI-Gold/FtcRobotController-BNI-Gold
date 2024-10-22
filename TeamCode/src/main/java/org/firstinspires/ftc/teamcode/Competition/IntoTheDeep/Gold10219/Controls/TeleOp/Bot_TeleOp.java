9fda1dfe98085c1ac638c4abcd2c26179579aad1
Mitchell Peck <50884550+MitchellPeck@users.noreply.github.com>
2024-10-22 12:26:46 -0400

    we have 5 turn torque servos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Gold10219/Controls/TeleOp/Bot_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Gold10219/Controls/TeleOp/Bot_TeleOp.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-import org.firstinspires.ftc.teamcode.Competition.IntoTheDeep.Gold10219.Robots.C1CompBot;
 import org.firstinspires.ftc.teamcode.Competition.IntoTheDeep.Gold10219.Robots.CompBot;
 
 @Disabled
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Gold10219/Testers/PosClawMechanismsTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Gold10219/Testers/PosClawMechanismsTester.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.Competition.IntoTheDeep.Gold10219.Robots.CompBot;
 
-@Disabled
 @TeleOp(name = "B - Claw Mechanisms Tester")
 public class PosClawMechanismsTester extends OpMode {
 
