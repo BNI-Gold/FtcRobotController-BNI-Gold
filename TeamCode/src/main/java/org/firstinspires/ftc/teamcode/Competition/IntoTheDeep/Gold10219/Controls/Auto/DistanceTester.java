649218af62ea31da986881e85809c5ebfe2ac33a
bni-gold <bnigold@mtbethelchristian.org>
2024-11-09 12:18:27 -0500

    val updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Gold10219/Controls/Auto/DistanceTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Gold10219/Controls/Auto/DistanceTester.java
@@ -16,14 +16,23 @@ public class DistanceTester extends AutoMain{
         while (opModeIsActive()) {
 
             //START AUTO PATH SEQUENCE
+            //1 Rotation is 6"
             Bot.strafeRight(0.5, 1);
             sleep(500);
+            //.5 Rotation is 3"
             Bot.strafeLeft(0.5, 0.5);
             sleep(1000);
 
+            //1 Rotation is 6"
             Bot.driveForward(0.5, 1);
             sleep(500);
+            //.5 Rotation is 3"
             Bot.driveBack(0.5, 0.5);
+            sleep(500);
+
+            // END AUTO PATH SEQUENCE
+            requestOpModeStop();
         }
+        idle();
     }
 }
