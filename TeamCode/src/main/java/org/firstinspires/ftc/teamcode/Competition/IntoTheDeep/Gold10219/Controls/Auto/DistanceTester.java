98f2a83944788d519756291f89618b24888011bd
bni-gold <bnigold@mtbethelchristian.org>
2024-11-09 12:18:38 -0500

    dis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Gold10219/Controls/Auto/DistanceTester.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Competition/IntoTheDeep/Gold10219/Controls/Auto/DistanceTester.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.Competition.IntoTheDeep.Gold10219.Controls.Auto;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
+@Disabled
 @Autonomous(name = "DistanceTester")
 public class DistanceTester extends AutoMain{
 
